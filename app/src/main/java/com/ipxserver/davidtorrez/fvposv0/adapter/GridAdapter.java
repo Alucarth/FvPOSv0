c2b1c91809e5951061cd029bdc6bb527b0c711c9
David <ltorrezs2008@gmail.com>
2015-05-19 12:13:48 -0400

    Control de seleccion de venta terminado XD prosegui con el flujo y los web services

--- app/src/main/java/com/ipxserver/davidtorrez/fvposv0/adapter/GridAdapter.java
+++ app/src/main/java/com/ipxserver/davidtorrez/fvposv0/adapter/GridAdapter.java
@@ -3,13 +3,16 @@ package com.ipxserver.davidtorrez.fvposv0.adapter;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.ColorMatrix;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.ipxserver.davidtorrez.fvposv0.R;
@@ -26,6 +29,8 @@ public class GridAdapter extends BaseAdapter
     Context context;
     TextView tv;
     TextView tc;
+    ImageView img;
+
     public GridAdapter(Context context){
         this.context = context;
         lista = new ArrayList<Product>();
@@ -63,22 +68,37 @@ public class GridAdapter extends BaseAdapter
         {
             rootView = inflater.inflate(R.layout.gridview_item,viewGroup,false);
         }
-        Product producto = lista.get(i);
+        final Product producto = lista.get(i);
 
-       tv= (TextView) rootView.findViewById(R.id.txtNotes);
+        final int contador =i;
+
+        tv= (TextView) rootView.findViewById(R.id.txtNotes);
         tv.setText(producto.getNotes());
         tc = (TextView) rootView.findViewById(R.id.txtCost);
 //        tc.setText(producto.getCost()+"x"+producto.getQty());
-        ImageButton b=(ImageButton) rootView.findViewById(R.id.imgButton);
+        img = (ImageView) rootView.findViewById(R.id.img_ic_remove);
         if(Integer.parseInt(producto.getQty())>0)
         {
+            img.setVisibility(View.VISIBLE);
+            img.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.i("David","posision "+ contador);
+                    Product prod = (Product)lista.get(contador);
+                    int cantidad = Integer.parseInt(prod.getQty());
+                    cantidad--;
+                    prod.setQty(""+cantidad);
+                    notifyDataSetChanged();
+
+                }
+            });
             tc.setText(producto.getCost() + "x" + producto.getQty());
-            rootView.setBackgroundColor(Color.GREEN);
-            b.setVisibility(View.VISIBLE);
+            rootView.setBackgroundColor(Color.BLUE);
+
         }else{
             tc.setText(producto.getCost());
-            rootView.setBackgroundColor(Color.BLUE);
-            b.setVisibility(View.INVISIBLE);
+            rootView.setBackgroundColor(Color.LTGRAY);
+            img.setVisibility(View.INVISIBLE);
         }
 
 
